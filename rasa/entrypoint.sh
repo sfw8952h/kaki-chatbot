bb875cb69819dd2b3ef9d187a339c43675f75432
Hugo Lopez <hugolopez@outlook.sg>
2025-12-16 03:28:19 +0800

    Log models in entrypoint

--- rasa/entrypoint.sh
+++ rasa/entrypoint.sh
@@ -4,6 +4,9 @@ set -e
 # Render provides $PORT; default to 5005 for local use.
 PORT="${PORT:-5005}"
 
+echo "Listing /app/models before start:"
+ls -lah /app/models || true
+
 exec rasa run \
   --enable-api \
   --cors "*" \
