343cb2c969949895b99cba7dac8375d6d18467cd
Hugo Lopez <hugolopez@outlook.sg>
2025-12-17 05:56:23 +0800

    bind port to 8080

--- rasa/entrypoint.sh
+++ rasa/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-# Render provides $PORT; default to 5005 for local use.
-PORT="${PORT:-5005}"
+# Render provides $PORT; default to 8080 for local use.
+PORT="${PORT:-8080}"
 
 echo "Listing /app/models before start:"
 ls -lah /app/models || true
@@ -10,6 +10,7 @@ ls -lah /app/models || true
 exec rasa run \
   --enable-api \
   --cors "*" \
+  --host "0.0.0.0" \
   --port "$PORT" \
   --endpoints endpoints.yml \
   --model /app/models/production.tar.gz
